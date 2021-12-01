f0c5d9cc4dbe3ab771c463b8122e2ebc40056a03
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2021-12-01 13:14:26 -0300

    UI update.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,40 +1,66 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   @override
-  _HomeState createState() {
-    return _HomeState();
-  }
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = ["a", "b", "c"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FloatingActionButton"),
-      ),
-      body: Text("Conteudo"),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: FloatingActionButton(
+        title: Text("Lista de tarefas"),
         backgroundColor: Colors.purple,
-        foregroundColor: Colors.white,
-        elevation: 6,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
         onPressed: () {
-          //print("Resultado");
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    FlatButton(
+                      child: Text("Cancelar"),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    FlatButton(
+                      child: Text("Salvar"),
+                      onPressed: () {
+                        //salvar
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                );
+              });
         },
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Row(
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.menu),
-            )
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaTarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_listaTarefas[index]),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
