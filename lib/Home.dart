e3375e08e1a7a0fd84be6336df004e85384b09d8
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2021-12-02 14:19:10 -0300

    Screen update.

--- lib/Home.dart
+++ lib/Home.dart
@@ -11,46 +11,53 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _listaTarefas = [];
+  TextEditingController _controllerTarefa = TextEditingController();
 
   //descobre o diretório onde o arquivo será salvo
-   _getFile() async {
+  _getFile() async {
     final diretorio = await getApplicationDocumentsDirectory();
     //print("Caminho:" + diretorio.path);
     var arquivo = File("${diretorio.path}/dados.json");
   }
 
-  _salvarArquivo() async {
-    var arquivo = await _getFile();
+  //add task in the task list.
+  _salvarTarefa() {
+    //recover input text.
+    String textoDigitado = _controllerTarefa.text;
     Map<String, dynamic> tarefa = Map();
-    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["titulo"] = textoDigitado;
     tarefa["realizada"] = false;
 
-    _listaTarefas.add(tarefa);
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+    _salvarArquivo();
+    _controllerTarefa.text = "";
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
 
     //convert to json
     String dados = json.encode(_listaTarefas);
     arquivo.writeAsString(dados);
   }
 
-
-  _lerArquivo()async{
-    try{
-
+  _lerArquivo() async {
+    try {
       //file recover
       final arquivo = await _getFile();
       return arquivo.readAsString();
-
-    }catch(error){
+    } catch (error) {
       return null;
     }
-
   }
 
   //data recover
   @override
   void initState() {
     super.initState();
-    _lerArquivo().then((dados){
+    _lerArquivo().then((dados) {
       setState(() {
         _listaTarefas = json.decode(dados);
       });
@@ -60,7 +67,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     //_salvarArquivo();
-    print("itens: "+_listaTarefas.toString());
+    print("itens: " + _listaTarefas.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas"),
@@ -77,6 +84,7 @@ class _HomeState extends State<Home> {
                 return AlertDialog(
                   title: Text("Adicionar Tarefa"),
                   content: TextField(
+                    controller: _controllerTarefa,
                     decoration: InputDecoration(labelText: "Digite sua tarefa"),
                     onChanged: (text) {},
                   ),
@@ -89,6 +97,7 @@ class _HomeState extends State<Home> {
                       child: Text("Salvar"),
                       onPressed: () {
                         //salvar
+                        _salvarTarefa();
                         Navigator.pop(context);
                       },
                     )
@@ -103,9 +112,19 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               itemCount: _listaTarefas.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                title: Text(_listaTarefas[index]["titulo"]),
+                return CheckboxListTile(
+                  title: Text(_listaTarefas[index]['titulo']),
+                  value: _listaTarefas[index]['realizada'],
+                  onChanged: (valorAlterado) {
+                    setState(() {
+                      _listaTarefas[index]['realizada'] = valorAlterado;
+                    });
+                    _salvarArquivo();
+                  },
                 );
+                /* return ListTile(
+                  title: Text(_listaTarefas[index]["titulo"]),
+                ); */
               },
             ),
           )
